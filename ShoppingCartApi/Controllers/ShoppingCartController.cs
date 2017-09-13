873dc6090c7a39a6ba65038ca07dc2b9d0c0759b
AsmaRafiq <asmarafiq@live.com>
2017-09-13 11:32:27 +0500

    ShoppingCartApi

--- ShoppingCartApi/Controllers/ShoppingCartController.cs
+++ ShoppingCartApi/Controllers/ShoppingCartController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-
+using System.Web;
 using System.Web.Http;
 
 namespace ShoppingCartApi.Controllers
 {
+    public class Item
+    {
+        public int Count { get; set; }
+
+        public Product Product { get; set; }
+    }
+    public class ShoppingCart
+    {
+        public List<Item> Items { get; set; }
+
+    }
     public class ShoppingCartController : ApiController
     {
         [Route("api/GetProducts")]
@@ -19,5 +30,40 @@ namespace ShoppingCartApi.Controllers
 
             return db.Products.ToList();
         }
+        [HttpGet]
+        [Route("api/AddToCart/{Id}")]
+        public void AddToCart(int Id)
+        {
+            ShoppingCart sc = null;
+
+            if (HttpContext.Current.Session["sc"] == null)
+                sc = new ShoppingCart();
+            else
+                sc = HttpContext.Current.Session["sc"] as ShoppingCart;
+
+            if (sc.Items == null)
+                sc.Items = new List<Item>();
+
+            var db = new ShoppingCartEntities();
+            db.Configuration.ProxyCreationEnabled = false;
+
+            var prod = db.Products.Find(Id);
+
+            Item item = new Item();
+            item.Count = 1;
+            item.Product = prod;
+
+            sc.Items.Add(item);
+
+            HttpContext.Current.Session["sc"] = sc;
+        }
+
+        [HttpGet]
+        [Route("api/GetCart")]
+        public ShoppingCart GetCart()
+        {
+            return null;
+        }
+
     }
 }
